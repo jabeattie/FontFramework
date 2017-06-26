b28f1e1af11c21f982310037f1e2ce09c484797a
James Beattie <james.andrew.beattie@gmail.com>
2017-06-26 20:32:11 +0100

    Added Framework

---
+++ FontFramework/FontFramework.h
@@ -0,0 +1,19 @@
+//
+//  FontFramework.h
+//  FontFramework
+//
+//  Created by James Beattie on 26/06/2017.
+//  Copyright © 2017 James Beattie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FontFramework.
+FOUNDATION_EXPORT double FontFrameworkVersionNumber;
+
+//! Project version string for FontFramework.
+FOUNDATION_EXPORT const unsigned char FontFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontFramework/PublicHeader.h>
+
+
